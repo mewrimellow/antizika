c1c610c93888e3580099f2bb0641a8bb95428a7e
mewrimellow <mewri@outlook.es>
2025-03-26 17:15:55 -0300

    Principal

--- antizika/Principal.cs
+++ antizika/Principal.cs
@@ -4,6 +4,10 @@ namespace antizika
 {
     public partial class Form1 : Form
     {
+        //Conexão com banco de dados
+        private readonly string _conexao = antizika.Properties.Settings.Default.Conexao;
+
+
         public Form1()
         {
             InitializeComponent();
